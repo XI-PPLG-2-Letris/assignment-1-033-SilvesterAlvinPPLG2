948e8d8ae9c563072a008591dea8efd313d0d671
SilvesterAlvinPPLG2 <silvestermanoppo@gmail.com>
2025-10-04 21:21:55 +0700

    tugas Mini Kakulator

---
+++ script.js
@@ -0,0 +1,12 @@
+let a = 2;
+let b = 4;
+
+const aritmatika = (a,b) => {
+    console.log(`ini adalah operator Penjumlahan ${a} + ${b} = ${a+b}`);
+    console.log(`ini adalah operator Pengurangan ${a} - ${b} = ${a-b}`);
+    console.log(`ini adalah operator Perkalian ${a} * ${b} = ${a*b}`);
+    console.log(`ini adalah operator Pembagian ${a} / ${b} = ${a/b}`);
+    console.log(`ini adalah operator Perpangkatan ${a} ** ${b} = ${a**b}`);
+    console.log(`ini adalah operator Hasil sisa Bagi ${a} % ${b} = ${a%b}`);
+}
+aritmatika(a,b)
